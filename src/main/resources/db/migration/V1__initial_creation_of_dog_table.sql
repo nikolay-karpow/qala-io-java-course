516beddcb79aae3ee423b510c3c5d87089296406
Nikolai Karpov <nikolay.karpow@gmail.com>
2017-08-17 23:21:56 +0300

    Add Flyway

---
+++ src/main/resources/db/migration/V1__initial_creation_of_dog_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Dog (
+id   INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(100),
+birthday TIMESTAMP,
+height INT,
+weight INT
+)
--- src/test/resources/ddl.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE Dog (id   INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), birthday TIMESTAMP, height INT, weight INT)
